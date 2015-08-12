c865d7d8ea6edbb04e63c1e72ab8d38f50575e2d
Lev Barkov <levbarkov@MacBook-Air-Lev.local>
2015-08-12 14:14:32 +0800

    Initial Commit

---
+++ v2 12082015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  v2 12082015
+//
+//  Created by Lev Barkov on 8/12/15.
+//  Copyright (c) 2015 Lev Barkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
